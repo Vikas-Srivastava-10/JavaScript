5acb8ae58b781d41d469802e50dd76ecf8bf3b7c
Vikas Srivastava <vikas200027@gmail.com>
2025-07-03 15:01:35 +0000

    Conversion of Data Types.

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33abc"
+console.log(typeof score);
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); //it converts but it is a NaN
+console.log(valueInNumber); //NaN
+
+//"33" => 33
+//"33abc" => NaN - Not a Number
+// true => 1; false => 0 -- viceversa
+// "" => false
+// "vikas" => true
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 12
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
