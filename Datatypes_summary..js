31be33a484a702c12c8f0d674338489ea3d389b2
Vikas Srivastava <vikas200027@gmail.com>
2025-07-04 04:12:45 +0000

    datatype summary

---
+++ Datatypes_summary..js
@@ -0,0 +1,10 @@
+// there are two types Primitive and Non - primitive
+
+// **********Primitive*************
+
+//7 Types = String, Number, Boolean, undefined, null, Symbol, BigInt
+
+
+//Reference (Non- Primitive)
+
+//Array, Object, Function
