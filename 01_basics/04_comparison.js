9308b8daa7ea1b3ecb0e8eaa9fe681753463e50f
Vikas Srivastava <vikas200027@gmail.com>
2025-07-04 04:13:09 +0000

    comparison in JS

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,25 @@
+console.log(2>1);
+
+console.log("2" > 1)  
+//js already converts "2" in integer 2
+console.log("02" > 1)
+
+console.log(null>0); //false
+console.log(null == 0); //false
+console.log(null>=0); //true
+
+
+console.log(undefined ==0); //false
+console.log(undefined > 0); //false
+console.log(undefined >=0); //false
+
+//=== strict check
+
+console.log("2" === 2); // it checks the entity and data type too
+
+
+
+
+
+
+
